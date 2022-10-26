02accc004ae006664a82e0f3a441043742c896e4
Romain Gehrig <romain.gehrig@gmail.com>
2022-10-26 17:03:29 +0200

    Better implementation of AttrTree using __getattr__

--- my/utils/tests/test_tree.py
+++ my/utils/tests/test_tree.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, List, Set
 import pytest
 
 from my.utils import AttrTree, AttrTreeConfig
+from my.utils.tree import AttrTree2
 
 
 @dataclass
@@ -15,7 +16,10 @@ class Dog:
 
     @property
     def race_hierarchy(self) -> List[str]:
-        return self.race.split(".")
+        if self.race:
+            return self.race.split(".")
+        else:
+            return []
 
     @property
     def hierarchy(self) -> List[str]:
@@ -52,33 +56,66 @@ def dogs(request, simple_dogs, duplicate_dogs):
     return [simple_dogs, duplicate_dogs][request.param]
 
 
-def create_dog_tree(dogs: List[Dog]) -> AttrTree[Dog]:
-    tree = AttrTree(
-        name="__root__",
-        root=True,
+def create_dog_tree(dogs: List[Dog]) -> AttrTree2[Dog]:
+    tree = AttrTree2(
         config=AttrTreeConfig(expose_leafs_items=True, item_name=lambda x: x.name, item_value=lambda x: x),
     )
     for dog in dogs:
-        tree.add_item(dog, hierarchy=dog.race_hierarchy)
+        tree.add_item(dog, ".".join(dog.race_hierarchy))
 
     return tree
 
+def create_dog_tree_no_leaf(dogs: List[Dog]) -> AttrTree2[Dog]:
+    tree = AttrTree2(
+        config=AttrTreeConfig(expose_leafs_items=False, item_name=lambda x: x.name, item_value=lambda x: x),
+    )
+    for dog in dogs:
+        tree.add_item(dog, ".".join(dog.race_hierarchy))
 
-def test_retrieve_dog_with_full_path(dogs):
-    dog_tree = create_dog_tree(dogs)
+    return tree
+
+@pytest.fixture(params=[0, 1])
+def dog_tree_creator(request):
+    return [create_dog_tree, create_dog_tree_no_leaf][request.param]
+
+def test_dog_hierarchy():
+    assert Dog(name="Abc", race="", age=0).full_name == "Abc"
+    assert Dog(name="Abc", race="abc", age=0).full_name == "abc.Abc"
+    assert Dog(name="Abc", race="a.b.c", age=0).full_name == "a.b.c.Abc"
+
+
+def test_as_list(dogs, dog_tree_creator):
+    dog_tree = dog_tree_creator(dogs)
+    dog_tree_list = dog_tree.as_list()
+    for dog in dogs:
+        assert (dog.full_name, dog) in dog_tree_list
+
+
+def test_length(dogs, dog_tree_creator):
+    dog_tree = dog_tree_creator(dogs)
+    assert len(dog_tree) == len(dogs)
+
+def test_exposed(simple_dogs):
+    dog_tree = create_dog_tree_no_leaf(simple_dogs)
+    assert dog_tree.__dir__() == ['Rouky']
+    assert dog_tree.bully.__dir__() == ['frenchy', 'Lucky']
+
+
+def test_retrieve_dog_with_full_path(dogs, dog_tree_creator):
+    dog_tree = dog_tree_creator(dogs)
     for dog in dogs:
         assert dog_tree[dog.full_name] == dog
 
 
-def test_retrieve_dog_only_with_name_no_duplicate(simple_dogs):
-    dog_tree = create_dog_tree(simple_dogs)
+def test_retrieve_dog_only_with_name_no_duplicate(simple_dogs, dog_tree_creator):
+    dog_tree = dog_tree_creator(simple_dogs)
     for dog in simple_dogs:
         assert getattr(dog_tree, dog.name) == dog
 
 
-def test_retrieve_dog_walking_down_the_tree_with_attributes(dogs):
-    dog_tree = create_dog_tree(dogs)
-    for dog in dogs:
+def test_retrieve_dog_walking_down_the_tree_with_attributes_no_duplicate(simple_dogs, dog_tree_creator):
+    dog_tree = dog_tree_creator(simple_dogs)
+    for dog in simple_dogs:
         current_node = dog_tree
 
         for next_elem in dog.hierarchy:
@@ -103,4 +140,4 @@ def test_retrieve_dog_only_with_name_does_not_work_with_duplicates(duplicate_dog
     dog_tree = create_dog_tree(duplicate_dogs)
     duplicate_names = [d.name for d in additional_dogs]
     for dog in duplicate_dogs:
-        assert getattr(dog_tree, dog.name) == dog or dog.name in duplicate_names
+        assert dog.name in duplicate_names or getattr(dog_tree, dog.name) == dog
--- my/utils/tree.py
+++ my/utils/tree.py
@@ -4,9 +4,12 @@ from argparse import ArgumentParser
 from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import (Any, Callable, Dict, Generator, Generic, List, Optional,
-                    Tuple, Type, TypeVar)
+                    Tuple, Type, TypeVar, Union)
 
 T = TypeVar("T")
+
+
+RecursiveDict = Dict[str, Union[T, 'RecursiveDict']]
 
 
 @dataclass
@@ -15,6 +18,120 @@ class AttrTreeConfig(Generic[T]):
     item_name: Callable[[T], str] = lambda x: x.name
     item_value: Callable[[T], Any] = lambda x: x.cls
 
+@dataclass
+class AttrItem(Generic[T]):
+    path: str
+    item_name: str
+    item: T
+
+    @property
+    def fullpath(self) -> str:
+        if self.path:
+            return self.path + "." + self.item_name
+        else:
+            return self.item_name
+
+
+@dataclass(frozen=True)
+class AttrTreeView(Generic[T]):
+    origin: 'AttrTree2'
+    path: str
+
+    def __dir__(self) -> List[str]:
+        return self.origin._exposed_elements(self.path)
+
+    def __getattr__(self, name):
+        fullname = self.path + "." + name
+        return self.origin.get_item(fullname)
+
+    def __str__(self):
+        return f"AttrTreeView(path={self.path}, items={self.__dir__()})"
+
+    def __repr__(self):
+        return f"AttrTreeView(path={self.path}, items={self.__dir__()})"
+
+
+@dataclass
+class AttrTree2(Generic[T]):
+    config: AttrTreeConfig[T]
+    _exposed: Dict[str, T] = field(default_factory=dict, init=False)
+    _items: List[AttrItem[T]] = field(default_factory=list, init=False)
+
+    def add_item(self, item: T, path):
+        new_item = AttrItem(path=path, item_name=self.config.item_name(item), item=item)
+        self._exposed[new_item.fullpath] = self.config.item_value(item)
+        self._items.append(new_item)
+
+    def _element_is_item(self, fullname):
+        return fullname in self._exposed
+
+    def _exposed_elements(self, path: str) -> List[str]:
+        exposed = list(set(k[len(path):] for k in self._exposed.keys() if k.startswith(path)))
+        exposed = [ s[1 if s.startswith(".") else 0:] for s in exposed ]
+        if not self.config.expose_leafs_items:
+            modules = set()
+            items = set()
+            for m in exposed:
+                *module, name = m.split('.')
+                if module:
+                    modules.add(module[0])
+                else:
+                    items.add(name)
+
+            exposed = list(modules) + list(items)
+        return exposed
+
+
+    def _path_is_module(self, path: str) -> bool:
+        return any(k.startswith(path) and k != path for k in self._exposed.keys())
+
+    def get_partial_name(self, fullpath) -> T:
+        *path, name = fullpath.split('.')
+        path = ".".join(path)
+        keys = [k for k in self._exposed.keys() if k.startswith(path) and k.endswith(name)]
+        if len(keys) == 1:
+            return self.get_item(keys[0])
+        else:
+            raise AttributeError()
+
+    def get_item(self, fullpath) -> Union[T, AttrTreeView[T]]:
+        if fullpath in self._exposed:
+            return self._exposed[fullpath]
+        elif self._path_is_module(fullpath):
+            return AttrTreeView(origin=self, path=fullpath)
+        return self.get_partial_name(fullpath)
+
+    def __getitem__(self, name):
+        return getattr(self, name)
+
+    def __getattr__(self, name):
+        return self.get_item(name)
+
+    def __dir__(self) -> List[str]:
+        d = super().__dir__()
+        return d + self._exposed_elements("")
+
+    def __len__(self) -> int:
+        return len(self._exposed)
+
+    def as_list(self) -> List[Tuple[str, T]]:
+        return list(self._exposed.items())
+
+    # def add_arguments(
+    #     self, parser: ArgumentParser, path: List[str], process_parser_kwargs: Dict[str, Any] = {}, **kwargs
+    # ):
+    #     this_parser = parser.add_subparsers(title=self.name)
+
+    #     # Add exposed
+    #     for process_name, external_process in self.exposed.items():
+    #         process_parser = this_parser.add_parser(process_name, **process_parser_kwargs)
+    #         process_parser.set_defaults(function_name=".".join(path + [process_name]))
+    #         external_process.process.add_arguments(process_parser, **kwargs)
+
+    #     for leaf in self.leafs.values():
+    #         leaf_parser = this_parser.add_parser(leaf.name)
+    #         leaf_parser.set_defaults(function_name=".".join(path + [leaf.name]))
+    #         leaf.add_arguments(leaf_parser, path + [leaf.name], process_parser_kwargs=process_parser_kwargs, **kwargs)
 
 @dataclass
 class AttrTree(Generic[T]):
@@ -29,11 +146,12 @@ class AttrTree(Generic[T]):
         assert (
             name not in self.leafs
         ), f"Cannot add item with name '{name}' ({item}) because it has the same name as a group."
+
         if not only_attr:
             self.exposed[name] = item
-
         if self.config.expose_leafs_items:
-            setattr(self, name, self.config.item_value(item))
+            value = self.config.item_value(item)
+            setattr(self, name, value)
 
     def add_item(self, item: T, hierarchy: Optional[List[str]]) -> Tuple[Optional[str], "AttrTree"]:
         self._expose_item(item, only_attr=bool(hierarchy))
