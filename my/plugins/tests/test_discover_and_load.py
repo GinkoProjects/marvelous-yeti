6f067cd60080df280945cb8fbb60bde541ea16c3
Romain Gehrig <romain.gehrig@gmail.com>
2022-11-13 08:31:44 +0100

    Add tests for loading an entrypoint

--- my/plugins/tests/test_discover_and_load.py
+++ my/plugins/tests/test_discover_and_load.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from dataclasses import dataclass
 from typing import Any, Dict, List, Set
 
@@ -6,10 +7,28 @@ import pytest
 
 from my.commands import Command
 from my.commands.tests.test_arguments import check_action_attributes
-from my.plugins.load import ExternalProcess, Plugin
+from my.plugins.load import ExternalProcess, Plugin, PluginLoader
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import EntryPoint
+else:
+    from importlib.metadata import EntryPoint
+
+
+def this_is_a_function():
+    return "Definitely!"
+
+
+def expose_as_entrypoint(name, group, fn) -> EntryPoint:
+    assert fn.__name__ not in globals() or globals()[fn.__name__] == fn
+    globals()[fn.__name__] = fn
+    entrypoint_value = f"{fn.__module__}:{fn.__name__}"
+    return EntryPoint(name=name, group=group, value=entrypoint_value)
 
 
 def test_process_tree_example():
+    """An example of a plugin exposing processes with multiple levels"""
+
     @dataclass
     class A(Command):
         i: int
@@ -43,3 +62,23 @@ def test_process_tree_example():
     }
 
     assert check_action_attributes(expected_arguments, parser._actions, excluded=set(["help"]))
+
+
+def test_entrypoint_creation():
+    e = expose_as_entrypoint("name", "my.group", this_is_a_function)
+    fn = e.load()
+    assert fn() == "Definitely!"
+
+
+def test_plugin_loader_command():
+    @dataclass
+    class A(Command):
+        i: int
+
+    cmd_entrypoint = expose_as_entrypoint("Printer", "my.plugins.command", A)
+
+    p = PluginLoader()
+    p.load_command(cmd_entrypoint)
+
+    assert "test_discover_and_load" in p.plugins
+    assert p.plugins["test_discover_and_load"].commands.as_list() == [("Printer", A)]
